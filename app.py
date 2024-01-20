998b4240acf741d8ef234fa19feeaa6259327b74
Keagan Rodrigues <rodrikea@sheridancollege.ca>
2024-01-20 13:31:11 -0500

    Template for app inplemented

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+template_folder = os.path.join(script_dir, 'template')
+static_folder = os.path.join(script_dir, 'static')
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder, static_url_path='/static')
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
